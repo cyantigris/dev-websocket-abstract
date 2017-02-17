52424d7f46eb9245867e339acd8ce4a73badefb4
jeff <jeff@swarmgate.com>
2017-02-16 21:07:40 -0800

    fix bug

--- Websocket_abstract/Websocket_abstract.js
+++ Websocket_abstract/Websocket_abstract.js
@@ -11,48 +11,57 @@ module.exports = class Websocket_abstract{
     }
 
     _init(){
+        console.log(this._socket);
+        if(typeof(WebSocket)!==undefined){
+            this._socket.on('connection',(_socket)=>{
+                this._setConnection(_socket);
+            });
+        }else{
+            this._socket.on('open',(_socket)=>{
+                this._setConnection(_socket);
+            });
+        }
+        console.log(`websocket started on port: ${this._port}`);
+    }
+
+    _setConnection(_socket){
         let _this = this,
             onMsgHandle = this._getProp('_onMsg'),
             onConnectionHandle = this._getProp('_onConnection'),
             onCloseHandle = this._getProp('_onClose');
-        
-        this._socket.on('connection',(_socket)=>{
+
+        if(_this.getProp('_debugMode')){
+            console.log('socket connect:',_socket);
+        }
+
+        if(socketType === 'server'){
+            _this.clientList.push(_socket);
+        }
+
+        if(onConnectionHandle !== null){
+            onConnectionHandle.call(_this,_socket);
+        }
+
+        _socket.on('message',(msg)=>{
+            
+            if(onMsgHandle !== null){
+                onMsgHandle.call(_this,msg);
+            }
 
             if(_this.getProp('_debugMode')){
-                console.log('socket connect:',_socket);
+                console.log('msg recevied:',msg);
             }
-
-            if(socketType === 'server'){
-                _this.clientList.push(_socket);
-            }
-            
-            if(onConnectionHandle !== null){
-                onConnectionHandle.call(_this,_socket);
-            }
-            
-            _socket.on('message',(msg)=>{
-                
-                if(onMsgHandle !== null){
-                    onMsgHandle.call(_this,msg);
-                }
-
-                if(_this.getProp('_debugMode')){
-                    console.log('msg recevied:',msg);
-                }
-            });
-
-            _socket.on('close',(msg)=>{
-                if(onCloseHandle !== null){
-                    onCloseHandle.call(_this,msg);
-                }
-
-                if(_this.getProp('_debugMode')){
-                    console.log('disconnented:',msg);
-                }
-            });
         });
 
-        console.log(`websocket started on port: ${this._port}`);
+        _socket.on('close',(msg)=>{
+            if(onCloseHandle !== null){
+                onCloseHandle.call(_this,msg);
+            }
+
+            if(_this.getProp('_debugMode')){
+                console.log('disconnented:',msg);
+            }
+        });
     }
 
     _getProp(key){
