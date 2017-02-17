482f8988a59355eb7c3e2e252869ac1a447dc670
jeff <jeff@swarmgate.com>
2017-02-16 18:53:37 -0800

    init commit

---
+++ Websocket_abstract/Websocket_abstract.js
@@ -0,0 +1,78 @@
+let Ws = null,
+    socketType = null;
+
+if(typeof Websocket !== 'undefined'){
+    socketType = 'client';
+}else{
+    Ws = require('ws').Server;
+    socketType = 'server';
+}
+
+
+module.exports = class Websocket_abstract{
+    constructor(port,host,debug){
+        this._socket = socketType === 'server' ? new Ws({port:port}) : new Websocket(`${host}:${port}`);
+        this._port = port;
+        this._debugMode = debug === undefined ? false : debug;
+        this._clientList = [];
+        this._onMsg = null;
+        this._onConnection = null;
+        this._onClose = null;
+        this._init();
+    }
+
+    _init(){
+        let _this = this,
+            onMsgHandle = this._getProp('_onMsg'),
+            onConnectionHandle = this._getProp('_onConnection'),
+            onCloseHandle = this._getProp('_onClose');
+        
+        this._socket.on('connection',(_socket)=>{
+
+            if(_this.getProp('_debugMode')){
+                console.log('socket connect:',_socket);
+            }
+
+            if(socketType === 'server'){
+                _this.clientList.push(_socket);
+            }
+            
+            if(onConnectionHandle !== null){
+                onConnectionHandle.call(_this,_socket);
+            }
+            
+            _socket.on('message',(msg)=>{
+                
+                if(onMsgHandle !== null){
+                    onMsgHandle.call(_this,msg);
+                }
+
+                if(_this.getProp('_debugMode')){
+                    console.log('msg recevied:',msg);
+                }
+            });
+
+            _socket.on('close',(msg)=>{
+                if(onCloseHandle !== null){
+                    onCloseHandle.call(_this,msg);
+                }
+
+                if(_this.getProp('_debugMode')){
+                    console.log('disconnented:',msg);
+                }
+            });
+        });
+
+        console.log(`websocket started on port: ${this._port}`);
+    }
+
+    _getProp(key){
+        return this[key] !== undefined ? this[key] : null;
+    }
+
+    _setProp(key,value){
+        if(this[key]!==undefined){
+            this[key] = value;
+        }
+    }
+}
